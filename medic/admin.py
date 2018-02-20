c6c815315bb20a5c22abe1e99d0554245b0da0c3
shivam <shivamshukla@live.com>
2018-02-20 23:52:50 +0530

    created models | working admin interface

--- medic/admin.py
+++ medic/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import medicine,device
 # Register your models here.
+
+admin.site.register(medicine)
+admin.site.register(device)
--- medic/models.py
+++ medic/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class medicine(models.Model):
+    device_id = models.ForeignKey('device', on_delete=models.CASCADE)
+    med_name = models.CharField(max_length=200)
+    med_no = models.IntegerField()
+    morning = models.IntegerField()
+    afternoon = models.IntegerField()
+    evening = models.IntegerField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.med_name
+
+class device(models.Model):
+	name=models.CharField(max_length=200)
+	devid=models.IntegerField();
+
+
+	def __str__(self):
+		return self.name
